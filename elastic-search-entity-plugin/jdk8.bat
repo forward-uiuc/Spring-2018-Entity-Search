2f47cf84a90c81c7f96cedc066f89b766f798023
abhiKohar <abhinavkohar007@gmail.com>
2018-03-31 20:38:26 -0500

    commit with context window version 1

---
+++ elastic-search-entity-plugin/jdk8.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_161
+echo setting PATH
+set PATH=C:\Program Files\Java\jdk1.8.0_161\bin;%PATH%
+echo Display java version
+java -version
